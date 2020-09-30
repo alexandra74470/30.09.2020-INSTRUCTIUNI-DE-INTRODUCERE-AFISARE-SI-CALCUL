853d17237fcadf251c4918c78504bfed09a00a2e
Alexandra Juravets <70714347+alexandra74470@users.noreply.github.com>
2020-09-30 13:39:57 +0300

    Add files via upload
    Intr-un autobuz care pleaca in excursie sunt 7 copii. De la inca 2 scoli urca alti copii, numarul acestora citindu-se la tastatura. Cati copii au plecat in excursie? Exemplu: date de intrare 15 20; date de iesire: 42 copii

---
+++ Problema3.py
@@ -0,0 +1,4 @@
+s1=int(input("dati numarul de copii ce urca la statia numarul 1"))
+s2=int(input("dati numarul de copii ce urca la statia numarul 2"))
+nt=7+s1+s2
+print("in excursie au plecat",nt,"copii")
